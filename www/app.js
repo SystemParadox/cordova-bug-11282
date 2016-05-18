5000088cd56931edc3098f4ccbe691254480476e
Simon Williams <simon@systemparadox.co.uk>
2016-05-18 11:00:51 +0100

    Add files via upload

---
+++ www/app.js
@@ -0,0 +1,9 @@
+// npm install react react-dom bluebird
+// browserify --debug app.js -o bundle.js
+// The resulting bundle size is 2.1Mb
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('bluebird');
+
+console.log('\n\n\n\n\nhello');
